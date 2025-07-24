2f921ddcafc069ad660f32ba3a2fa35ed5358a0b
anjaanid <sujj4646@gmail.com>
2025-07-24 20:07:24 +0530

    fixed mongoose version issue

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
   console.log(`${process.env.MONGO_DB_URL}/${DB_NAME}`);
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGO_DB_URL}/${DB_NAME}`
+      `${process.env.MONGO_DB_URL}/${DB_NAME}`,
+      { family: 4 }
     );
     console.log(
       "Connected to mongoDB Successfully, The host name is",
